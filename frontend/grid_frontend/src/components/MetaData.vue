9af55c7908a5dc9d590738b714e1a0bf9102b9fa
Your Name <you@example.com>
2023-08-19 19:16:10 +0530

    combined metadata with user form

--- frontend/grid_frontend/src/components/MetaData.vue
+++ frontend/grid_frontend/src/components/MetaData.vue
@@ -1,238 +1,294 @@
 <template>
-    <div class="main-box">
-        <div class="info-box">
-            <div class = "metadata-box"><UserForm/></div>
-            <div class ="chat-box" ref="scrollableDiv">
-                <div v-if="conversation_history.length === 0">
-                    <h1 style="color:yellowgreen; text-align: center; padding-top:22% ;">Your conversation with Flipbot will appear here</h1>
-                </div>
-                <div v-for="prompt in conversation_history" :key="prompt" class="conversation">
-                    <div v-if="prompt.role === 'user'" class="checkbox-user">
-                        <p>YOU</p>
-                        <div class="user-chat">
-                            {{ prompt.content }}
-                        </div>
-                    </div>
-                    <div v-else class="checkbox-gpt">
-                        <p>FLIPBOT</p>
-                        <div class="gpt-chat">{{ prompt.content }}</div>
-                    </div>
-                </div>
-            </div>
+  <div class="main-box">
+    <div class="info-box">
+      <div class="metadata-box">
+        <UserForm @form-submitted="handleChildData" />
+      </div>
+      <div class="chat-box" ref="scrollableDiv">
+        <div v-if="conversation_history.length === 0">
+          <h1 style="color: yellowgreen; text-align: center; padding-top: 22%">
+            Your conversation with Flipbot will appear here
+          </h1>
         </div>
-        <div class = "product-box">
-            <div class="history">
-                <div v-if="products.length === 0" class="if-zero">
-                    <h1 style="color: yellowgreen; text-align: center;">Welcome to Flipbot !!!</h1>
-                    <h1 style="color: yellowgreen; text-align: center;">
-                        There are no products to display, start chatting with the flipbot in the textbox below to display products that you like !!!
-                        <br/><br/>Before you start, please enter all your details in the left so that we can give you an accurate fashion outfit.
-                        <br/><br/>You can also give us an image so that we can find out outfits that are similar to it.
-                        <br/><br/>Happy Shopping !!!
-                    </h1>
-                </div>
-                <div class="allProducts">
-                    <ProductCard v-for="product in products" :key="product"/>
-                </div>
+        <div
+          v-for="prompt in conversation_history"
+          :key="prompt"
+          class="conversation"
+        >
+          <div v-if="prompt.role === 'user'" class="checkbox-user">
+            <p>YOU</p>
+            <div class="user-chat">
+              {{ prompt.content }}
             </div>
-            <div class="text-box">
-                <div><textarea placeholder="Start the conversation here!!!" class="chatbox-input" v-model = "prompt"/></div>
-                <div><button type="submit" @click="sendPrompt">Submit</button></div>
-            </div>
+          </div>
+          <div v-else class="checkbox-gpt">
+            <p>FLIPBOT</p>
+            <div class="gpt-chat">{{ prompt.content }}</div>
+          </div>
         </div>
+      </div>
     </div>
+    <div class="product-box">
+      <div class="history">
+        <div v-if="products.length === 0" class="if-zero">
+          <h1 style="color: yellowgreen; text-align: center">
+            Welcome to Flipbot !!!
+          </h1>
+          <h1 style="color: yellowgreen; text-align: center">
+            There are no products to display, start chatting with the flipbot in
+            the textbox below to display products that you like !!!
+            <br /><br />Before you start, please enter all your details in the
+            left so that we can give you an accurate fashion outfit.
+            <br /><br />You can also give us an image so that we can find out
+            outfits that are similar to it. <br /><br />Happy Shopping !!!
+          </h1>
+        </div>
+        <div class="allProducts">
+          <ProductCard
+            v-for="product in products"
+            :key="product"
+            :url="getImageUrl(product)"
+          />
+        </div>
+      </div>
+      <div class="text-box">
+        <div>
+          <textarea
+            placeholder="Start the conversation here!!!"
+            class="chatbox-input"
+            v-model="prompt"
+          />
+        </div>
+        <div><button type="submit" @click="sendPrompt">Submit</button></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import ProductCard from './ProductCard.vue'
-    import UserForm from './UserForm.vue'
-    export default{
-        name : 'MetaData',
-        submit:'Submit',
-        components:{
+import axios from "axios";
+import ProductCard from "./ProductCard.vue";
+import UserForm from "./UserForm.vue";
+export default {
+  name: "MetaData",
+  submit: "Submit",
+  components: {
     UserForm,
     ProductCard,
-},
-        data(){
-            return{
-                prompt: '',
-                count:0,
-                conversation_history: [],
-                products:[]
-            }
-        },
-        methods : {
-            sendPrompt(){
-                // TODO Send prompt data to backend
-                    console.log(this.prompt)
-                    // this.products.push(this.count);
-                    // this.count++;
-                    this.conversation_history.push({'role':'user', 'content':this.prompt})
-                    this.$nextTick(() => {
-                        this.scrollToEnd();
-                    });
-                },
-                scrollToEnd() {
-                    const scrollableDiv = this.$refs.scrollableDiv;
-                    scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
-                }
-            },
-        }
+  },
+  data() {
+    return {
+      prompt: "",
+      count: 0,
+      conversation_history: [],
+      products: [],
+      location: "",
+      productCount: 1,
+      age: 1,
+      gender: "",
+      similarity: 0.5,
+    };
+  },
+  methods: {
+    handleChildData(formData) {
+      this.location = formData.location;
+      this.productCount = formData.productCount;
+      this.age = formData.age;
+      this.similarity = formData.similarity;
+      this.gender = formData.gender;
+    },
+
+    async sendPrompt() {
+      // TODO Send prompt data to backend
+      console.log(this.prompt);
+      console.log(
+        this.location,
+        this.productCount,
+        this.age,
+        this.gender,
+        this.similarity
+      );
+      try {
+        const response = await axios.get("URL_OF_BACKEND");
+        this.products = response.data;
+      } catch (error) {
+        console.error("Error fetching images:", error);
+      }
+
+      this.conversation_history.push({ role: "user", content: this.prompt });
+      this.conversation_history.push({
+        role: "gpt",
+        content: "hello" /* GPT PROMPT TO BE ADDED */,
+      });
+      this.$nextTick(() => {
+        this.scrollToEnd();
+      });
+    },
+
+    getImageUrl(imageName) {
+      return `../../../../matches/${imageName}`; // Replace with your image directory path
+    },
+
+    scrollToEnd() {
+      const scrollableDiv = this.$refs.scrollableDiv;
+      scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
+    },
+  },
+};
 </script>
 
 <style>
+.allProducts {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  justify-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+  scrollbar-width: thin;
+  scrollbar-color: #e0e0e0 #f0f0f0;
+}
 
-    .allProducts{
-        display: flex;
-        flex-wrap: wrap;
-        gap:10px;
-        justify-items: center;
-        justify-content: space-evenly;
-        width: 100%;
-        height: 100%;
-        overflow-y:auto;
-        overflow-x: hidden;
-        scrollbar-width: thin;
-        scrollbar-color: #e0e0e0 #f0f0f0;
-    }
+.allProducts::-webkit-scrollbar {
+  width: 6px;
+  background-color: grey;
+}
 
-    .allProducts::-webkit-scrollbar{
-        width: 6px;
-        background-color: grey;
-    }
+.allProducts::-webkit-scrollbar-thumb {
+  background-color: white;
+  border-radius: 3px;
+}
 
-    .allProducts::-webkit-scrollbar-thumb {
-        background-color: white;
-        border-radius: 3px;
-    }
+.allProducts::-webkit-scrollbar-thumb:hover {
+  background-color: white;
+}
 
-    .allProducts::-webkit-scrollbar-thumb:hover {
-        background-color: white;
-    }
+.main-box {
+  width: 95rem;
+  height: 46rem;
+  display: flex;
+}
 
-    .main-box{
-        width:95rem;
-        height:46rem;
-        display: flex;  
-    }
+.text-box {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  box-sizing: border-box;
+  margin-bottom: 1vh;
+}
 
-    .text-box{
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-        box-sizing: border-box;
-        margin-bottom: 1vh;
-    }
+.checkbox-user {
+  margin-left: auto;
+}
 
-    .checkbox-user{
-        margin-left: auto;
-    }
+.checkbox-gpt {
+  margin-right: auto;
+}
 
-    .checkbox-gpt{
-        margin-right: auto;
-    }
+.info-box {
+  width: 55vh;
+  height: 93%;
+  margin: 1.5rem 1.5rem 1.5rem 1.5rem;
+  border-radius: 10px;
+  border: 5px solid silver;
+  color: white;
+  display: flex;
+  flex-direction: column;
+}
 
-    .info-box{
-        width:55vh;
-        height:93%;
-        margin: 1.5rem 1.5rem 1.5rem 1.5rem;
-        border-radius: 10px;
-        border: 5px solid silver;
-        color: white;
-        display: flex;
-        flex-direction: column;
-    }
+.conversation {
+  display: flex;
+  flex-direction: row;
+  height: auto;
+  width: 55vh;
+  padding: 10px;
+}
 
-    .conversation{
-        display: flex;
-        flex-direction: row;
-        height: auto;
-        width: 55vh;
-        padding: 10px;
-    }
+.user-chat {
+  margin-left: auto;
+  padding: 2vh 2vh 2vh 2vh;
+  border-radius: 10px;
+  margin-right: 2.5vh;
+  background-color: yellowgreen;
+}
 
-    .user-chat{
-        margin-left: auto;
-        padding: 2vh 2vh 2vh 2vh;
-        border-radius: 10px;
-        margin-right: 2.5vh;
-        background-color: yellowgreen;
-    }
+.gpt-chat {
+  margin-left: auto;
+  padding: 2vh 2vh 2vh 2vh;
+  border-radius: 10px;
+  background-color: crimson;
+}
 
-    .gpt-chat{
-        margin-left: auto;
-        padding: 2vh 2vh 2vh 2vh;
-        border-radius: 10px;
-        background-color: crimson;
-    }
+.product-box {
+  display: flex;
+  flex-direction: column;
+  width: 65%;
+  height: 93%;
+  margin: 1.5rem 1.5rem 1.5rem 1.5rem;
+  border-radius: 10px;
+  border: 5px solid silver;
+}
 
-    .product-box{
-        display: flex;
-        flex-direction: column;
-        width: 65%;
-        height:93%;
-        margin: 1.5rem 1.5rem 1.5rem 1.5rem;
-        border-radius: 10px;
-        border: 5px solid silver;
-    }
-    
-    .chat-box{
-        width: 100%;
-        height: 48vh;
-        overflow-y:auto;
-        overflow-x: hidden;
-        scrollbar-width: thin;
-        scrollbar-color: #e0e0e0 #f0f0f0;
-    }
+.chat-box {
+  width: 100%;
+  height: 48vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  scrollbar-width: thin;
+  scrollbar-color: #e0e0e0 #f0f0f0;
+}
 
-    .chat-box::-webkit-scrollbar{
-        width: 6px;
-        background-color: grey;
-    }
+.chat-box::-webkit-scrollbar {
+  width: 6px;
+  background-color: grey;
+}
 
-    .chat-box::-webkit-scrollbar-thumb {
-        background-color: white;
-        border-radius: 3px;
-    }
+.chat-box::-webkit-scrollbar-thumb {
+  background-color: white;
+  border-radius: 3px;
+}
 
-    .chat-box::-webkit-scrollbar-thumb:hover {
-        background-color: white;
-    }
+.chat-box::-webkit-scrollbar-thumb:hover {
+  background-color: white;
+}
 
-    .metadata-box{
-        width: 100%;
-        height: 50%;
-    }
+.metadata-box {
+  width: 100%;
+  height: 50%;
+}
 
-    .chatbox-input{
-        width: 110vh;
-        padding: 10px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        font-size: 16px;
-        box-sizing: border-box;
-        resize: horizontal;
-        font-family:  'Roboto', sans-serif;
-    }
+.chatbox-input {
+  width: 110vh;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  box-sizing: border-box;
+  resize: horizontal;
+  font-family: "Roboto", sans-serif;
+}
 
-    button{
-        padding: 10px 20px;
-        background-color: #007bff;
-        color: #fff;
-        border: none;
-        border-radius: 5px;
-        font-size: 16px;
-        cursor: pointer;
-    }
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+}
 
-    .chatbox-input::placeholder {
-        font-family:  'Roboto', sans-serif;
-    }
+.chatbox-input::placeholder {
+  font-family: "Roboto", sans-serif;
+}
 
-    .history{
-        width: 95%;
-        height: 95%;
-        margin: 2.5% 2.5% 2.5% 2.5%;
-        overflow-y: hidden;
-    }
+.history {
+  width: 95%;
+  height: 95%;
+  margin: 2.5% 2.5% 2.5% 2.5%;
+  overflow-y: hidden;
+}
 </style>
--- frontend/grid_frontend/src/components/UserForm.vue
+++ frontend/grid_frontend/src/components/UserForm.vue
@@ -1,262 +1,312 @@
 <template>
-<div class="user-data">
-    <div class="heading"><h3><u>Please Enter Your Data</u></h3>
+  <div class="user-data">
+    <div class="heading">
+      <h3><u>Please Enter Your Data</u></h3>
     </div>
     <div class="location">
-        <div class="location2">
-            <label for="location">Location:&nbsp;</label>
-            <input type="text" id="location" v-model="location" placeholder="Enter Your Location" class="location-input">
-        </div>
+      <div class="location2">
+        <label for="location">Location:&nbsp;</label>
+        <input
+          type="text"
+          id="location"
+          v-model="formData.location"
+          placeholder="Enter Your Location"
+          class="location-input"
+        />
+      </div>
     </div>
 
     <div class="age">
-        <div class="age2">
-            <label for="age">Age:&nbsp;</label>
-            <input type="number" id="age" v-model.number="age" class="age-input" min="1" max="99">
-        </div>
+      <div class="age2">
+        <label for="age">Age:&nbsp;</label>
+        <input
+          type="number"
+          id="age"
+          v-model.number="formData.age"
+          class="age-input"
+          min="1"
+          max="99"
+        />
+      </div>
     </div>
 
     <div class="gender">
-        <div class="gender2">
-            <label for="gender">Gender:&nbsp;</label>
-            <select id="gender" v-model="gender" class="select-gender">
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-            <option value="Unisex">Unisex</option>
-            </select>
-        </div>
+      <div class="gender2">
+        <label for="gender">Gender:&nbsp;</label>
+        <select id="gender" v-model="formData.gender" class="select-gender">
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Unisex">Unisex</option>
+        </select>
+      </div>
     </div>
 
     <div class="productCount">
-        <div class="productCount2">
-        <div class="productLabel"><label for="productCount">Number of Products:&nbsp;</label></div>
-        <div class="productInput"><input type="range" id="productCount" v-model="productCount" min="1" max="99"></div>
-        <div class="productValue"><span>&nbsp;{{ formattedSelectedValue }}</span></div>
+      <div class="productCount2">
+        <div class="productLabel">
+          <label for="productCount">Number of Products:&nbsp; Less </label>
         </div>
+        <div class="productInput">
+          <input
+            type="range"
+            id="productCount"
+            v-model="formData.productCount"
+            min="1"
+            max="99"
+          />
+        </div>
+        <div class="productLabel">More</div>
+      </div>
     </div>
 
     <div class="similarity-box">
-        <div class="similarity-box2">
-            <div class="productLabel"><label for="trendy">More Trendy&nbsp;</label></div>
-            <div class="productInput"><input type="range" id="trendy" v-model="similarity" step="0.01" min="0.5" max="0.8"></div>
-            <div class="productLabel"><label for="exact">&nbsp;More Exact</label></div>
+      <div class="similarity-box2">
+        <div class="productLabel">
+          <label for="trendy">More Trendy&nbsp;</label>
         </div>
+        <div class="productInput">
+          <input
+            type="range"
+            id="trendy"
+            v-model="formData.similarity"
+            step="0.01"
+            min="0.5"
+            max="0.8"
+          />
+        </div>
+        <div class="productLabel">
+          <label for="exact">&nbsp;More Exact</label>
+        </div>
+      </div>
     </div>
 
     <div class="image-upload-box">
-        <div class=image-upload-box2>
-            <div><label for="image" class="productLabel">Image</label></div>
-            <div><input type="file" name="image" accept="image/jpeg" class="imageInput"></div>
+      <div class="image-upload-box2">
+        <div><label for="image" class="productLabel">Image</label></div>
+        <div>
+          <input
+            type="file"
+            name="image"
+            accept="image/jpeg"
+            class="imageInput"
+          />
         </div>
+      </div>
     </div>
 
-    <button @click="submitForm" :class="[isSubmit ? 'submit' : 'submitted']">{{submit}}</button>
-</div>
+    <button @click="submitForm" :class="[isSubmit ? 'submit' : 'submitted']">
+      {{ submit }}
+    </button>
+  </div>
 </template>
-  
+
 <script>
+
 export default {
-    data() {
-        return {
-            location:'',
-            productCount:1,
-            age:1,
-            gender:'',
-            similarity:0.5,
-            submit:'Submit',
-            isSubmit:true
-        }
-    },
-    methods: {
-        submitForm() {
-            this.isSubmit = false;
-            if(!this.isSubmit && this.submit === 'Submitted'){
-                window.alert('Already submitted user data, please refresh to enter new user data');
-                return;
-            }
-            this.submit = 'Submitted'
-            //To do
-            // Send axios request to backend
-            console.log(this.location,this.productCount,this.age,this.gender,this.similarity)
-        }
-    },
-    computed: {
-        formattedSelectedValue() {
-          return String(this.productCount).padStart(2, '0');
-        }
+  data() {
+    return {
+      formData:{
+          location: "",
+          productCount: 1,
+          age: 1,
+          gender: "",
+          similarity: 0.5,
+      },
+      submit: "Submit",
+      isSubmit: true,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$emit('form-submitted',this.formData);
+      this.isSubmit = false;
+      if (!this.isSubmit && this.submit === "Submitted") {
+        window.alert(
+          "Already submitted user data, please refresh to enter new user data"
+        );
+        return;
       }
-}
+      this.submit = "Submitted";
+      console.log(
+        this.formData.location,
+        this.formData.productCount,
+        this.formData.age,
+        this.formData.gender,
+        this.formData.similarity
+      );
+    },
+  },
+};
 </script>
 
 <style>
-    .submit{
-        margin-bottom: 2%;
-    }
+.submit {
+  margin-bottom: 2%;
+}
 
-    .submitted{
-        margin-bottom: 2%;
-        background-color: yellowgreen;
-    }
+.submitted {
+  margin-bottom: 2%;
+  background-color: yellowgreen;
+}
 
-    .imageInput{
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        font-size: 16px;
-        box-sizing: border-box;
-        resize: horizontal;
-        font-family:  'Roboto', sans-serif;
-    }
+.imageInput {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  box-sizing: border-box;
+  resize: horizontal;
+  font-family: "Roboto", sans-serif;
+}
 
-    .heading{
-        width:99%;
-        text-align: center;
-    }
+.heading {
+  width: 99%;
+  text-align: center;
+}
 
-    .image-upload-box2{
-        padding: 0.5vh 0vh 0.5vh 0vh;
-        display: flex;
-        width: 99%;
-        flex-direction: row;
-        align-items: center;
-        justify-content:space-evenly;
-    }
+.image-upload-box2 {
+  padding: 0.5vh 0vh 0.5vh 0vh;
+  display: flex;
+  width: 99%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
 
-    .image-upload-box{
-        border-radius: 5px;
-        display: flex;
-        width: 99%;
-        flex-direction: row;
-        align-items: center;
-        border: 1px solid silver;
-    }
+.image-upload-box {
+  border-radius: 5px;
+  display: flex;
+  width: 99%;
+  flex-direction: row;
+  align-items: center;
+  border: 1px solid silver;
+}
 
-    .similarity-box2{
-        padding: 0.5vh 0vh 0.5vh 0vh;
-        display: flex;
-        width: 100%;
-        flex-direction: row;
-        align-items: center;
-        justify-content:space-evenly;
-    }
+.similarity-box2 {
+  padding: 0.5vh 0vh 0.5vh 0vh;
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
 
-    .similarity-box{
-        border-radius: 5px;
-        display: flex;
-        width: 99%;
-        flex-direction: row;
-        align-items: center;
-        border: 1px solid silver;
-    }
+.similarity-box {
+  border-radius: 5px;
+  display: flex;
+  width: 99%;
+  flex-direction: row;
+  align-items: center;
+  border: 1px solid silver;
+}
 
-    .user-data{
-        font-size: 16px;
-        width: 100%;
-        height: 100%;
-        border: 1px solid silver;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-        font-family:  'Roboto', sans-serif;
-        gap:1vh      
-    }
+.user-data {
+  font-size: 16px;
+  width: 100%;
+  height: 100%;
+  border: 1px solid silver;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  font-family: "Roboto", sans-serif;
+  gap: 1vh;
+}
 
-    .location-input{
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        font-size: 16px;
-        box-sizing: border-box;
-        resize: horizontal;
-        font-family:  'Roboto', sans-serif;
-    } 
+.location-input {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  box-sizing: border-box;
+  resize: horizontal;
+  font-family: "Roboto", sans-serif;
+}
 
-    .location2{
-        padding: 0.5vh 0vh 0.5vh 0vh;
-        display: flex;
-        width: 100%;
-        flex-direction: row;
-        align-items: center;
-        justify-content:space-evenly;
-    }
+.location2 {
+  padding: 0.5vh 0vh 0.5vh 0vh;
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
 
-    .location{
-        border-radius: 5px;
-        display: flex;
-        width: 99%;
-        flex-direction: row;
-        align-items: center;
-        border: 1px solid silver;
-    }
+.location {
+  border-radius: 5px;
+  display: flex;
+  width: 99%;
+  flex-direction: row;
+  align-items: center;
+  border: 1px solid silver;
+}
 
-    .gender2{
-        padding: 0.5vh 0vh 0.5vh 0vh;
-        display: flex;
-        width: 100%;
-        flex-direction: row;
-        align-items: center;
-        justify-content:space-evenly;
-    }
+.gender2 {
+  padding: 0.5vh 0vh 0.5vh 0vh;
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
 
-    .gender{
-        border-radius: 5px;
-        display: flex;
-        width: 99%;
-        flex-direction: row;
-        align-items: center;
-        border: 1px solid silver;
-    }
+.gender {
+  border-radius: 5px;
+  display: flex;
+  width: 99%;
+  flex-direction: row;
+  align-items: center;
+  border: 1px solid silver;
+}
 
-    .age2{
-        padding: 0.5vh 0vh 0.5vh 0vh;
-        display: flex;
-        width: 100%;
-        flex-direction: row;
-        align-items: center;
-        justify-content:space-evenly;
-    }
+.age2 {
+  padding: 0.5vh 0vh 0.5vh 0vh;
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
 
-    .age{
-        border-radius: 5px;
-        display: flex;
-        width: 99%;
-        flex-direction: row;
-        align-items: center;
-        border: 1px solid silver;
-    }
+.age {
+  border-radius: 5px;
+  display: flex;
+  width: 99%;
+  flex-direction: row;
+  align-items: center;
+  border: 1px solid silver;
+}
 
-    .age-input{
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        font-size: 16px;
-        box-sizing: border-box;
-        resize: horizontal;
-        font-family:  'Roboto', sans-serif;
-    }
+.age-input {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  box-sizing: border-box;
+  resize: horizontal;
+  font-family: "Roboto", sans-serif;
+}
 
-    .productCount{
-        border-radius: 5px;
-        border: 1px solid silver;
-        display: flex;
-        flex-direction: row;
-        width: 99%;
-    }
+.productCount {
+  border-radius: 5px;
+  border: 1px solid silver;
+  display: flex;
+  flex-direction: row;
+  width: 99%;
+}
 
-    .productCount2{
-        padding: 0.5vh 0vh 0.5vh 0vh;
-        display: flex;
-        width: 99%;
-        flex-direction: row;
-        align-items: center;
-        justify-content:space-evenly;
-    }
+.productCount2 {
+  padding: 0.5vh 0vh 0.5vh 0vh;
+  display: flex;
+  width: 99%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
 
-    .select-gender{
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        font-size: 16px;
-        box-sizing: border-box;
-        resize: horizontal;
-        font-family:  'Roboto', sans-serif;
-    }
-
+.select-gender {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  box-sizing: border-box;
+  resize: horizontal;
+  font-family: "Roboto", sans-serif;
+}
 </style>
